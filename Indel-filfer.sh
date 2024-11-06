64a98a0281c0d04fbc5761ed108c047ab18dd5e1
yufan <wangyufan22@mails.ucas.ac.cn>
2024-11-06 13:30:26 +0800

    Update Indel-filfer.sh

--- Indel-filfer.sh
+++ Indel-filfer.sh
@@ -6,6 +6,7 @@ gatk --java-options "-Djava.io.tmpdir=/data01/wangyf/project2/CyprinusCarpio/tmp
 
 gatk --java-options "-Djava.io.tmpdir=/data01/wangyf/project2/CyprinusCarpio/tmp -Xms80G -Xmx80G -XX:ParallelGCThreads=20" SelectVariants -R $genome -V raw.vcf.gz -select-type INDEL --restrict-alleles-to BIALLELIC -O raw.indel.vcf.gz
 
+:<<!
 gatk VariantFiltration --java-options "-Djava.io.tmpdir=/data01/wangyf/project2/CyprinusCarpio/tmp -Xms80G -Xmx80G -XX:ParallelGCThreads=20" \
 -R $genome -V raw.indel.vcf.gz -O raw.indel.QUAL30.QD2.FS200.SOR10.MQRankSum5.ReadPosRankSum8.vcf.gz \
 -filter "QUAL < 30" -filter-name "QUAL30" \
@@ -14,6 +15,9 @@ gatk VariantFiltration --java-options "-Djava.io.tmpdir=/data01/wangyf/project2/
 -filter "MQRankSum < 5" -filter-name "MQRankSum-5" \
 -filter "SOR > 10.0" -filter-name "SOR10" \
 -filter "ReadPosRankSum < -8.0" -filter-name "ReadPosRankS-8"
+!
 
 gatk SelectVariants --java-options "-Djava.io.tmpdir=/data01/wangyf/project2/CyprinusCarpio/tmp -Xms80G -Xmx80G -XX:ParallelGCThreads=20" --exclude-filtered true -V raw.indel.QUAL30.QD2.FS200.SOR10.MQRankSum5.ReadPosRankSum8.vcf.gz -O raw.indel.QUAL30.QD2.FS200.SOR10.MQRankSum5.ReadPosRankSum8.PASS.vcf.gz
 
+## get InDel region
+perl /data01/wangyf/software/annovar/convert2annovar.pl -format vcf4old  raw.indel.maxmiss0.05.AF0.05.vcf.gz --outfile raw.indel.maxmiss0.05.AF0.05.vcf.gz.avinput
